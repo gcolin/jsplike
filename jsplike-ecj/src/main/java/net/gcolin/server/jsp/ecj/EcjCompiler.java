dc815f4d1c6e61b604d682bae1eade69ed7d7549
Gael COLIN <gael87@gmail.com>
2016-10-16 10:34:38 -0500

    eclipse compiler

---
+++ jsplike-ecj/src/main/java/net/gcolin/server/jsp/ecj/EcjCompiler.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package net.gcolin.server.jsp.ecj;
+
+import net.gcolin.server.jsp.internal.JdkCompiler;
+
+import org.eclipse.jdt.internal.compiler.tool.EclipseCompiler;
+
+import javax.tools.JavaCompiler;
+
+/**
+ * A compiler that uses the Eclipse Java Compiler.
+ * 
+ * @author Gaël COLIN
+ * @since 1.0
+ */
+public class EcjCompiler extends JdkCompiler {
+
+
+  @Override
+  protected JavaCompiler getJavaCompiler() {
+    return new EclipseCompiler();
+  }
+
+}
